d228e9bc2dfbb6f5b5cf19cc3e4cee2c2b390056
Amanjot-001 <amanjotsingh2309@gmail.com>
2023-12-09 02:27:41 +0530

    header created

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import '../assets/styles/app.css'
+import Header from './Header';
+
+const App = () => {
+    return (
+        <div className="app">
+            <Header />
+        </div>
+    )
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import '../assets/styles/header.css';
+import logo from '../assets/images/logo.png'
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo">
+                <p>overClock</p>
+                <img src={logo} alt="gpu-image" />
+            </div>
+            <div className="search-bar">
+                <input className='header-search' type="text" placeholder='search games'/>
+            </div>
+            <div className="navs">
+                <div className="sign-in">Sign-in</div>
+                <div className="curr-path">My library</div>
+                <div className="cart">cart</div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
