c6c0e17a57beb7bfa5ad621ce100a8dbd7887345
Amanjot-001 <amanjotsingh2309@gmail.com>
2023-12-12 21:17:49 +0530

    shimmer style updated

--- src/components/Shimmer.jsx
+++ src/components/Shimmer.jsx
@@ -1,11 +1,21 @@
 import '../assets/styles/shimmer.css'
 
 const Shimmer = () => {
+    const dummyCards = Array.from({ length: 9 }, (_, index) => (
+        <div key={index} className="dummy-card">
+            <div className="dummy-img"></div>
+            <div className="dummy-content">
+                <div className="dummy-top"></div>
+                <div className="dummy-mid"></div>
+                <div className="dummy-down"></div>
+            </div>
+        </div>
+    ));
+
     return (
         <div className="shimmer">
-            <div className="dummy-heading"></div>
             <div className="dummy-games">
-                hi
+                {dummyCards}
             </div>
         </div>
     )
