0c1671d1d69544a20cbaba57229286b84d6b17be
Amanjot-001 <amanjotsingh2309@gmail.com>
2023-12-16 17:58:27 +0530

    clicks fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,15 +3,36 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCartShopping, faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 const Header = () => {
     const [navbarOpen, setNavbarOpen] = useState(false);
     const cartItems = useSelector((store) => store.cart.items);
+    const navbarRef = useRef(null);
+    const buttonRef = useRef(null);
 
     const handleToggleBtn = () => {
         setNavbarOpen((prev) => !prev);
     }
+
+    useEffect(() => {
+        const handler = (event) => {
+            if (
+                navbarRef.current &&
+                !navbarRef.current.contains(event.target) &&
+                buttonRef.current &&
+                !buttonRef.current.contains(event.target)
+            ) {
+                setNavbarOpen(false);
+            }
+        };
+
+        document.addEventListener('mousedown', handler);
+
+        return () => {
+            document.removeEventListener('mousedown', handler);
+        };
+    }, [navbarOpen]);
 
     return (
         <div className="header">
@@ -22,17 +43,21 @@ const Header = () => {
                 <input className='header-search' type="text" placeholder='search games' />
             </div>
 
-            <button 
+            <button
                 className='toggle-btn'
                 onClick={handleToggleBtn}
+                ref={buttonRef}
             >
                 <FontAwesomeIcon icon={!navbarOpen ? faBars : faXmark} />
             </button>
 
-            <div className={`navs ${navbarOpen ? 'show-nav' : ''}`}>
-                <div className="sign-in">Sign-in</div>
-                <div className="curr-path">My library</div>
-                <NavLink to='/cart' className='links'>
+            <div
+                className={`navs ${navbarOpen ? 'show-nav' : ''}`}
+                ref={navbarRef}
+            >
+                <div className="sign-in" onClick={() => setNavbarOpen(false)}>Sign-in</div>
+                <div className="curr-path" onClick={() => setNavbarOpen(false)}>My library</div>
+                <NavLink to='/cart' className='links' onClick={() => setNavbarOpen(false)}>
                     <div className="cart">
                         <FontAwesomeIcon icon={faCartShopping} />
                         ({cartItems.length})
