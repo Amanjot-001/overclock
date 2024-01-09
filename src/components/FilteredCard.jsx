df97b9539f13ffabaaf0dc81b763790ede88f7b2
Amanjot-001 <amanjotsingh2309@gmail.com>
2024-01-09 20:09:08 +0530

    fix: array mapping

--- src/components/FilteredCard.jsx
+++ src/components/FilteredCard.jsx
@@ -7,64 +7,64 @@ import Pagination from './Pagination';
 import Shimmer from './Shimmer';
 
 const FilteredCard = () => {
-    const [data, setData] = useState(null);
-    const [currentPage, setCurrentPage] = useState(1);
-    const { timeFrame } = useParams();
+  const [data, setData] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const { timeFrame } = useParams();
 
-    const API_TO_USE = useFilteredCard(timeFrame);
+  const API_TO_USE = useFilteredCard(timeFrame);
 
-    useEffect(() => {
-        setCurrentPage(1);
-    }, [timeFrame]);
+  useEffect(() => {
+    setCurrentPage(1);
+  }, [timeFrame]);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                setData(null);
-                const response = await fetch(`${API_TO_USE}&page=${currentPage}&page_size=40`);
-                const res = await response.json();
-                setData(res);
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        };
-
-        fetchData();
-
-        return () => {
-            // setCurrentPage(1);
-            setData(null);
-        }
-    }, [API_TO_USE, currentPage, timeFrame])
-
-    const handlePageChange = (newPage) => {
-        setCurrentPage(newPage);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setData(null);
+        const response = await fetch(`${API_TO_USE}&page=${currentPage}&page_size=40`);
+        const res = await response.json();
+        setData(res);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
     };
 
-    return (
-        <div className="display">
-            <h1 className='display-heading'>{timeFrame}</h1>
-            {data ? (
-                <div className="games">
-                    {Array.isArray(data.results) && data.results.length > 0 ? (
-                        data.results.map((game) => <GameCard key={game.id} data={game} />)
-                    ) : (
-                        <h1>No content available</h1>
-                    )}
-                </div>
-            ) : (
-                <Shimmer />
-            )}
+    fetchData();
 
-            {data && data.count > 40 && (   
-                <Pagination 
-                    currentPage={currentPage} 
-                    totalPages={Math.min(Math.floor(data.count / 40), 250)} 
-                    onPageChange={handlePageChange} 
-                />
-            )}
+    return () => {
+      // setCurrentPage(1);
+      setData(null);
+    }
+  }, [API_TO_USE, currentPage, timeFrame])
+
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
+
+  return (
+    <div className="display">
+      <h1 className='display-heading'>{timeFrame}</h1>
+      {data ? (
+        <div className="games">
+          {Array.isArray(data.results) && data.results.length > 0 ? (
+            data.results.map((game) => <GameCard key={game.id} data={game} />)
+          ) : (
+            <h1>No content available</h1>
+          )}
         </div>
-    )
+      ) : (
+        <Shimmer />
+      )}
+
+      {data && data.count > 40 && (
+        <Pagination
+          currentPage={currentPage}
+          totalPages={Math.min(Math.floor(data.count / 40), 250)}
+          onPageChange={handlePageChange}
+        />
+      )}
+    </div>
+  )
 }
 
 export default FilteredCard;
--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -16,175 +16,175 @@ const FakeContainer = styled.div`
 `;
 
 const GameCard = ({ data }) => {
-    const [bgimgLoaded, setBgimgLoaded] = useState(false);
-    const [lastHoveredImage, setLastHoveredImage] = useState(-1);
-    const [fakeHeight, setFakeHeight] = useState(null);
-    const [outOfCard, setOutOfCard] = useState(false);
-    const cardRef = useRef(null);
+  const [bgimgLoaded, setBgimgLoaded] = useState(false);
+  const [lastHoveredImage, setLastHoveredImage] = useState(-1);
+  const [fakeHeight, setFakeHeight] = useState(null);
+  const [outOfCard, setOutOfCard] = useState(false);
+  const cardRef = useRef(null);
 
-    const rating = getRatingIcon(data.ratings[0]?.title);
-    const metaRating = data.metacritic;
+  const rating = getRatingIcon(data.ratings[0]?.title);
+  const metaRating = data.metacritic;
 
-    const nintendoIcon = useMemo(() => <img className='switch-icon' src={nintendo} alt="Nintendo Switch" />, []);
+  const nintendoIcon = useMemo(() => <img className='switch-icon' src={nintendo} alt="Nintendo Switch" />, []);
 
-    const cartItems = useSelector((store) => store.cart.items);
+  const cartItems = useSelector((store) => store.cart.items);
 
-    const isAdded = cartItems.some(item => item.id === data.id);
+  const isAdded = cartItems.some(item => item.id === data.id);
 
-    const dispatch = useDispatch();
-    const handleAddItem = (dataToChange) => {
-        if (!isAdded)
-            dispatch(addItem(dataToChange));
-        else
-            dispatch(removeItem(dataToChange));
+  const dispatch = useDispatch();
+  const handleAddItem = (dataToChange) => {
+    if (!isAdded)
+      dispatch(addItem(dataToChange));
+    else
+      dispatch(removeItem(dataToChange));
+  }
+
+  const updateHeight = () => {
+    if (cardRef.current) {
+      const cardHeight = cardRef.current.offsetHeight;
+      setFakeHeight(cardHeight);
     }
+  };
 
-    const updateHeight = () => {
-        if (cardRef.current) {
-            const cardHeight = cardRef.current.offsetHeight;
-            setFakeHeight(cardHeight);
-        }
+  const handleImageHover = (itemId) => {
+    setLastHoveredImage(itemId);
+  };
+
+  const handleMouseEvent = (e) => {
+    if (e.target !== cardRef.current) {
+      return;
+    }
+    updateHeight();
+    setOutOfCard(prev => !prev);
+  };
+
+  useEffect(() => {
+    updateHeight();
+
+    window.addEventListener('resize', updateHeight);
+
+    return () => {
+      window.removeEventListener('resize', updateHeight);
     };
+  }, [bgimgLoaded, outOfCard]);
 
-    const handleImageHover = (itemId) => {
-        setLastHoveredImage(itemId);
-    };
-
-    const handleMouseEvent = (e) => {
-        if (e.target !== cardRef.current) {
-            return;
-        }
-        updateHeight();
-        setOutOfCard(prev => !prev);
-    };
-
-    useEffect(() => {
-        updateHeight();
-
-        window.addEventListener('resize', updateHeight);
-
-        return () => {
-            window.removeEventListener('resize', updateHeight);
-        };
-    }, [bgimgLoaded, outOfCard]);
-
-    return (
-        <FakeContainer className='fake' height={fakeHeight}>
-            <div
-                className="game-card"
-                ref={cardRef}
-                // onMouseEnter={setOutOfCard(prev => !prev)}
-                onMouseLeave={handleMouseEvent}
-            >
-                {data ? (
-                    <>
-                        <div className="game-img">
-                            {!bgimgLoaded ?
-                                <img
-                                    src={defaultImg}
-                                    alt="game-img"
-                                    className='default-image'
-                                    onLoad={() => setBgimgLoaded(true)}
-                                />
-                                :
-                                <div className="all-images">
-                                    <img
-                                        src={data?.short_screenshots.find(item => item.id === lastHoveredImage)?.image || data.background_image}
-                                        alt={`screenshot`}
-                                        onLoad={() => setBgimgLoaded(true)}
-                                    />
-                                    <div className="image-btns" onMouseLeave={() => handleImageHover(-1)}                                    >
-                                        {data.short_screenshots.map((item) => (
-                                            <div
-                                                key={item.id}
-                                                className={`image-key ${lastHoveredImage === item.id ? 'image-key-active' : ''}`}
-                                                onMouseEnter={() => handleImageHover(item.id)}
-                                            >
-                                                <div></div>
-                                            </div>
-                                        ))}
-                                    </div>
-                                </div>
-                            }
-                        </div>
-                        <div className="content">
-                            <div className="top-sec">
-                                <div className="platform">
-                                    {data.parent_platforms.map((platform) => {
-                                        const icon = getPlatformIcon(platform.platform.slug);
-                                        return icon !== null ? (
-                                            <span key={platform.platform.id}>
-                                                {icon === 'nintendo' ? nintendoIcon : <FontAwesomeIcon icon={icon} />}
-                                            </span>
-                                        ) : null;
-                                    })}
-                                </div>
-                                {metaRating ?
-                                    <div className="meta-rating">
-                                        {metaRating}
-                                    </div>
-                                    :
-                                    null
-                                }
-                            </div>
-                            <div className="name">
-                                {data.name}
-                            </div>
-                            <div className="mid-sec">
-                                <div className="rating">
-                                    <img className='rating-icon' src={rating} alt="rating-icon" />
-                                </div>
-                                <div className="wishlist-btn">
-                                    wishlist <FontAwesomeIcon className='wishlist-icon' icon={faGift} />
-                                </div>
-                            </div>
-                            <div className="down-sec">
-                                <div className="follow">
-                                    +{data.added}
-                                </div>
-                                <div className={`add-btn ${isAdded ? 'added' : null}`} onClick={() => handleAddItem(data)}>
-                                    {isAdded ? 'Remove' : 'Add'}
-                                    <FontAwesomeIcon className='cart-icon' icon={faCartShopping} />
-                                </div>
-                            </div>
-                            {bgimgLoaded &&
-                                <div className="extra-sec">
-                                    <div className="released">
-                                        <span>Release date:</span>
-                                        <span>{data.released}</span>
-                                    </div>
-                                    <hr />
-                                    <div className="game-genre">
-                                        <span>Genres:</span>
-                                        <span className='all-genres'>
-                                            {data.genres.slice(0, 3).map((gameGenre, index) => (
-                                                <React.Fragment key={gameGenre.id}>
-                                                    {index > 0 && ', '}
-                                                    {gameGenre.name}
-                                                </React.Fragment>
-                                            ))}
-                                            {data.genres.length > 3 && '...'}
-                                        </span>
-                                    </div>
-                                    <hr />
-                                    <div className="see-more-btn">
-                                        <span>See more</span>
-                                        <span><FontAwesomeIcon icon={faChevronRight} /></span>
-                                    </div>
-                                </div>
-                            }
-                        </div>
-                    </>
-                ) : (
-                    <h1 className='no-content-heading'>No content available</h1>
-                )}
+  return (
+    <FakeContainer className='fake' height={fakeHeight}>
+      <div
+        className="game-card"
+        ref={cardRef}
+        // onMouseEnter={setOutOfCard(prev => !prev)}
+        onMouseLeave={handleMouseEvent}
+      >
+        {data ? (
+          <>
+            <div className="game-img">
+              {!bgimgLoaded ?
+                <img
+                  src={defaultImg}
+                  alt="game-img"
+                  className='default-image'
+                  onLoad={() => setBgimgLoaded(true)}
+                />
+                :
+                <div className="all-images">
+                  <img
+                    src={data?.short_screenshots.find(item => item.id === lastHoveredImage)?.image || data.background_image}
+                    alt={`screenshot`}
+                    onLoad={() => setBgimgLoaded(true)}
+                  />
+                  <div className="image-btns" onMouseLeave={() => handleImageHover(-1)}                                    >
+                    {Array.isArray(data.short_screenshots) && data.short_screenshots.map((item) => (
+                      <div
+                        key={item.id}
+                        className={`image-key ${lastHoveredImage === item.id ? 'image-key-active' : ''}`}
+                        onMouseEnter={() => handleImageHover(item.id)}
+                      >
+                        <div></div>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              }
             </div>
-        </FakeContainer>
-    )
+            <div className="content">
+              <div className="top-sec">
+                <div className="platform">
+                  {Array.isArray(data.parent_platforms) && data.parent_platforms.map((platform) => {
+                    const icon = getPlatformIcon(platform.platform.slug);
+                    return icon !== null ? (
+                      <span key={platform.platform.id}>
+                        {icon === 'nintendo' ? nintendoIcon : <FontAwesomeIcon icon={icon} />}
+                      </span>
+                    ) : null;
+                  })}
+                </div>
+                {metaRating ?
+                  <div className="meta-rating">
+                    {metaRating}
+                  </div>
+                  :
+                  null
+                }
+              </div>
+              <div className="name">
+                {data.name}
+              </div>
+              <div className="mid-sec">
+                <div className="rating">
+                  <img className='rating-icon' src={rating} alt="rating-icon" />
+                </div>
+                <div className="wishlist-btn">
+                  wishlist <FontAwesomeIcon className='wishlist-icon' icon={faGift} />
+                </div>
+              </div>
+              <div className="down-sec">
+                <div className="follow">
+                  +{data.added}
+                </div>
+                <div className={`add-btn ${isAdded ? 'added' : null}`} onClick={() => handleAddItem(data)}>
+                  {isAdded ? 'Remove' : 'Add'}
+                  <FontAwesomeIcon className='cart-icon' icon={faCartShopping} />
+                </div>
+              </div>
+              {bgimgLoaded &&
+                <div className="extra-sec">
+                  <div className="released">
+                    <span>Release date:</span>
+                    <span>{data.released}</span>
+                  </div>
+                  <hr />
+                  <div className="game-genre">
+                    <span>Genres:</span>
+                    <span className='all-genres'>
+                      {Array.isArray(data.genres) && data.genres.slice(0, 3).map((gameGenre, index) => (
+                        <React.Fragment key={gameGenre.id}>
+                          {index > 0 && ', '}
+                          {gameGenre.name}
+                        </React.Fragment>
+                      ))}
+                      {data.genres.length > 3 && '...'}
+                    </span>
+                  </div>
+                  <hr />
+                  <div className="see-more-btn">
+                    <span>See more</span>
+                    <span><FontAwesomeIcon icon={faChevronRight} /></span>
+                  </div>
+                </div>
+              }
+            </div>
+          </>
+        ) : (
+          <h1 className='no-content-heading'>No content available</h1>
+        )}
+      </div>
+    </FakeContainer>
+  )
 }
 
 GameCard.propTypes = {
-    data: PropTypes.object
+  data: PropTypes.object
 }
 
 export default GameCard;
